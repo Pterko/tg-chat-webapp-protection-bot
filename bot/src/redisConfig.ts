fca86124dc4251c11ec8291680bb0c491a327ed8
Aleksei Kolchanov <pter96@gmail.com>
2023-10-10 14:42:18 +0400

    Added support of DB option Redis

--- bot/src/redisConfig.ts
+++ bot/src/redisConfig.ts
@@ -2,5 +2,6 @@ export default {
   host: process.env.REDIS_HOST,
   port: parseInt(String(process.env.REDIS_PORT)),
   username:process.env.REDIS_USERNAME,
-  password:process.env.REDIS_PASSWORD
+  password:process.env.REDIS_PASSWORD,
+  db: Number(process.env.REDIS_DB || 0)
 }
